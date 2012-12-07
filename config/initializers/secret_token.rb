f4dcffd4144aa0acd531fb2f135113d3670ca4e3
allanlacaba <lacaba.allan@gmail.com>
2012-12-07 12:46:09 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemberMms::Application.config.secret_token = 'd95599098c94aa7d2524117dca4cd46b69df17f9c4b8e355cd02736f6ee943bdbe67f5ab56a074232a5f7e87938d02ccc8120b27bbc79805c3943d2214709322'
